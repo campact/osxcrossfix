34da998176e30b423cb562dca775a9aeba0635a1
Alexander Surma <surma@surmair.de>
2014-01-29 16:47:06 +0000

    Simply install go-bindata

--- compile_cas.sh
+++ compile_cas.sh
@@ -4,5 +4,6 @@
 curl http://curl.haxx.se/ca/cacert.pem > data/ca.pem
 
 # needs go-bindata from github.com/jteeuwen/go-bindata
+go get github.com/jteeuwen/go-bindata/go-bindata
 
 go-bindata -pkg=osxcrossfix -nomemcopy data
